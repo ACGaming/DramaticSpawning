57e5731e227c5141d6da82aaa01845f8290ef135
ACGaming <4818419+ACGaming@users.noreply.github.com>
2022-05-27 11:03:16 +0200

    Fix version

--- src/main/java/mod/acgaming/dramatic_spawning/DramaticSpawning.java
+++ src/main/java/mod/acgaming/dramatic_spawning/DramaticSpawning.java
@@ -11,7 +11,7 @@ public class DramaticSpawning
 {
     public static final String MOD_ID = "dramatic_spawning";
     public static final String NAME = "Dramatic Spawning";
-    public static final String VERSION = "1.12.2-1.0.0";
+    public static final String VERSION = "1.0.0";
     public static final Logger LOGGER = LogManager.getLogger("Dramatic Spawning");
 
     @Mod.EventHandler
